267facea20a3b3e0e10b15caf113e3d0d636382c
Julio Herrera <julio.herrera310@gmail.com>
2024-02-16 00:47:41 -0800

    Added tournament model and manager

--- models/tournament.py
+++ models/tournament.py
@@ -1,50 +1,60 @@
+import json
+import os
+
 class Tournament:
-    """Tournament model:
-        All tournaments have rounds associated with them.
-        All rounds have matches associated with them.
-
-        Attributes:
-        name (str): name of tournament.
-        venue (str): location of tournament.
-        start_date (str): day tournament started.
-        end_date (str): day tournament ended.
-        is_completed (bool): whether tournament is completed or not
-        registered_players (list): List of registered players.
-        unique_id (int): Tournament's unique id.
-        leaderboard (dict): Tournament's leaderboard.
-        num_rounds (int): Number of rounds.
-        current_round (int): current round of tournament
-        all_rounds (dict): All rounds associated with this tournament.
-    """
-
-    # constructor of Tournament
-    def __init__(self, name, venue, start_date, end_date, unique_id, registered_players: list,
-                 leaderboard: dict, is_completed: bool, num_rounds=0, current_round=0):
+    """This class contains information of a tournament, creates Tournament instances, loads and saves
+    and deletes them as well"""
+    def __init__(self, filepath=None, name=None):
 
         self.name = name
-        self.venue = venue
-        self.start_date = start_date
-        self.end_date = end_date
-        self.is_completed = is_completed
-        self.registered_players = registered_players
-        self.unique_id = unique_id
-        self.leaderboard = leaderboard
-        self.num_rounds = num_rounds
-        self.current_round = current_round
-        self.all_rounds = {}
+        self.start_date = None
+        self.end_date = None
+        self.venue = None
+        self.num_rounds = None
+        self.current_round = None
+        self.is_completed = None
+        self.registered_players = []
+        self.is_finished = None
+        self.rounds = []
+        self.filepath = filepath
+
+        if filepath and not name:
+            # Load data of tournament from JSON file
+            with open(filepath) as fp:
+                data = json.load(fp)
+                self.name = data.get("name")
+                dates = data.get("dates", {})
+                self.start_date = dates.get("from")
+                self.end_date = dates.get("to")
+                self.venue = data.get("venue")
+                self.num_rounds = data.get("number_of_rounds")
+                self.current_round = data.get("current_round")
+                self.is_completed = data.get("completed")
+                self.registered_players = data.get("players", [])
+                self.is_finished = data.get("finished")
+                self.rounds = data.get("rounds", [])
+
+        elif not filepath:
+            # we did not have a file, so we are going to create it by running save method
+            self.save()
 
     def __str__(self):
-        tournament_content = "Tournament name: {name}\n".format(name=self.name)
-        tournament_content += "- Venue: {location}\n".format(location=self.venue)
+        tournament_content = "- Tournament name: {name}\n".format(name=self.name)
         tournament_content += "- Start Date: {start_date}\n".format(start_date=self.start_date)
         tournament_content += "- End Date: {end_date}\n".format(end_date=self.end_date)
+        tournament_content += "- Venue: {location}\n".format(location=self.venue)
         tournament_content += "- Total Number of rounds: {num}\n".format(num=self.num_rounds)
+        tournament_content += "- Current Round: {curr}\n".format(curr=self.current_round)
+        tournament_content += "- Completed?: {completed}\n".format(completed=self.is_completed)
         tournament_content += "- Players: {players}\n".format(players=self.registered_players)
-        tournament_content += "- Leaderboard: {leaderboard}\n".format(leaderboard=self.leaderboard)
-
-        tournament_content += " - - Started rounds - -\n"
-
-        for started_round in self.all_rounds:
-            tournament_content += started_round.__str__()
+        tournament_content += "- Finished?: {finished}\n".format(finished=self.is_finished)
+        tournament_content += "- Rounds: {rounds}\n".format(rounds=self.rounds)
 
         return tournament_content
+
+    def save(self):
+        """This function serializes the Tournament information to the JSON file"""
+        with open(self.filepath, "w") as fp:
+            json.dump(
+
+            )
---
+++ models/tournament_manager.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+
+from .tournament import Tournament
+
+class TournamentManager:
+    """This class handles the proper creation of Tournament files and adds them to the data folder"""
+    def __init__(self, data_folder="data/tournaments"):
+        data_directory = Path(data_folder)
+        self.data_folder = data_directory
+        self.tournaments = []
+        for filepath in data_directory.iterdir():
+            if filepath.is_file() and filepath.suffix == ".json":
+                try:
+                    self.tournaments.append(Tournament(filepath))
+                except json.JSONDecodeError:
+                    print(filepath, "is an invalid JSON file.")
+
+    def create(self, name):
+        filepath = self.data_folder / (name.replace(" ","") + ".json")
+        tournament = Tournament(name=name, filepath=filepath)
+        tournament.save()
+
+        self.tournaments.append(tournament)
+        return tournament
